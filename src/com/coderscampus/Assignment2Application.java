6f5bc65bc113a8dbe5cbdddb129019ff9e648c82
HaydenHuffman <120144719+HaydenHuffman@users.noreply.github.com>
2023-09-25 19:36:45 -0400

    Bug Fixes & Refactor

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -5,35 +5,41 @@ import java.util.Scanner;
 
 public class Assignment2Application {
 
-	public static void main(String[] args) {
-		Random random = new Random();
-		int count = 0;
-		int answer = 1 + random.nextInt(99);
-		System.out.println("Pick a number between 1 and 100");
-		Scanner scanner = new Scanner(System.in);
-		int guess = scanner.nextInt();
-		while (guess < 1 || guess > 100) {
-			System.out.println("Your guess is not between 1 and 100, please try again");
-			guess = scanner.nextInt();
-		}
-		while (count < 5 ) {
-			if (guess > answer) {
-				System.out.println("Please pick a lower number");
-				count++;
-				guess = scanner.nextInt();
-			} else if (guess < answer) {
-				System.out.println("Please pick a higher number");
-				count++;
-				guess = scanner.nextInt();
-			} else {
-				System.out.println("You win!");
-				break;
-			}
-		}
-		if (count == 5) {
-			System.out.println("You lose, the number to guess was " + answer);
-		}
+    public static void main(String[] args) {
+        int count = 0;
+        Random random = new Random();
+        Integer answer = 1 + random.nextInt(99);
+        System.out.println("Pick a number between 1 and 100");
+        Boolean result = false;
+        while (count < 5 && !result) {
+            result = playGame(answer, count);
+            count++;
+        }
+        if (count == 5 && !result) {
+            System.out.println("You lose, the number to guess was " + answer);
+        }
+    }
 
-	}
+    public static Integer getGuess() {
+        Scanner scanner = new Scanner(System.in);
+        Integer guess = scanner.nextInt();
+        while (guess < 1 || guess > 100) {
+            System.out.println("Your guess is not between 1 and 100, please try again");
+            guess = scanner.nextInt(); // Read another guess
+        }
+        return guess;
+    }
 
+    public static Boolean playGame(Integer answer, Integer count) {
+        Integer guess = getGuess();
+        if (guess == answer) {
+            System.out.println("You win!");
+            return true;
+        } else if (guess > answer && count < 4) {
+            System.out.println("Please pick a lower number");
+        } else if (guess < answer && count < 4) {
+            System.out.println("Please pick a higher number");
+        }
+        return false;
+    }
 }
